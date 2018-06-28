ec63591c5139927ebf6ee9d722331704d43391c7
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-28 11:59:19 -0700

    Added comments

--- ToDoApi/ToDoApi/Controllers/ToDoController.cs
+++ ToDoApi/ToDoApi/Controllers/ToDoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
 using ToDoApi.Models;
 
@@ -13,18 +14,29 @@ namespace ToDoApi.Controllers
     public class ToDoController : ControllerBase
     {
         private readonly ToDoDbContext _context;
-
+        /// <summary>
+        /// This allows us to use dependency injection
+        /// </summary>
+        /// <param name="context">Our dbcontext variable</param>
         public ToDoController(ToDoDbContext context)
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Action that allows us to view all todo items
+        /// </summary>
+        /// <returns>List of ToDos</returns>
         [HttpGet]
         public ActionResult<List<ToDoItem>> GetAll()
         {
             return _context.ToDoItems.ToList();
         }
-
+        /// <summary>
+        /// Action that allows us to get a ToDo item
+        /// by its specific id
+        /// </summary>
+        /// <param name="id">Id of todo itme</param>
+        /// <returns>Null if it doesnt exist or the item</returns>
         [HttpGet("{id}", Name = "GetToDo")]
         public ActionResult<ToDoItem> GetById([FromRoute]long id)
         {
@@ -37,9 +49,13 @@ namespace ToDoApi.Controllers
             item.ToDoList = toDoList.Name;
             return item;
         }
-
+        /// <summary>
+        /// Action that allows us to create a new todo
+        /// </summary>
+        /// <param name="item">ToDoItem object of data from the body</param>
+        /// <returns>CreatedAtRoute status and redirects to a get</returns>
         [HttpPost]
-        public IActionResult Create(ToDoItem item)
+        public IActionResult Create([FromBody]ToDoItem item)
         {
             if(item.ListID == 0)
             {
@@ -50,26 +66,33 @@ namespace ToDoApi.Controllers
 
             return CreatedAtRoute("GetToDo", new { id = item.ID }, item);
         }
-
+        /// <summary>
+        /// Action that allows us to update a ToDo item
+        /// </summary>
+        /// <param name="id">specific todo item id</param>
+        /// <param name="item">ToDoItem object with data from the body</param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute]long id, [FromBody]ToDoItem item)
         {
-            var todo = await _context.ToDoItems.FindAsync(id);
+            ToDoItem todo = await _context.ToDoItems.FindAsync(id);
 
             if (todo == null)
             {
                 return RedirectToAction("Create", item);
             }
-
-            todo.IsDone = item.IsDone;
-            todo.Name = item.Name;
-            todo.ListID = item.ListID;
+            _context.Entry(todo).State = EntityState.Detached;
+            todo = item;
 
             _context.ToDoItems.Update(todo);
             await _context.SaveChangesAsync();
             return NoContent();
         }
-
+        /// <summary>
+        /// Action that allows us to remove a specific todo item
+        /// </summary>
+        /// <param name="id">id f todoitem</param>
+        /// <returns>NoContent if success, NotFound if null</returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute]long id)
         {
--- ToDoApi/ToDoApi/Controllers/ToDoListController.cs
+++ ToDoApi/ToDoApi/Controllers/ToDoListController.cs
@@ -14,18 +14,29 @@ namespace ToDoApi.Controllers
     public class ToDoListController : ControllerBase
     {
         private ToDoDbContext _context;
-
+        /// <summary>
+        /// This allows us to use dependency injection
+        /// </summary>
+        /// <param name="context">our dbcontext variable</param>
         public ToDoListController(ToDoDbContext context)
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Action that returns all available lists
+        /// </summary>
+        /// <returns>List of ToDoLists</returns>
         [HttpGet]
         public ActionResult<List<ToDoList>> GetAll()
         {
             return _context.ToDoLists.ToList();
         }
-
+        /// <summary>
+        /// Action that returns a specific list and what tasks
+        /// are associated with that list
+        /// </summary>
+        /// <param name="id">id of list</param>
+        /// <returns>ToDoList or NotFound</returns>
         [HttpGet("{id}", Name = "GetList")]
         public async Task<ActionResult<ToDoList>> GetById([FromRoute]int id)
         {
@@ -39,16 +50,25 @@ namespace ToDoApi.Controllers
 
             return Ok(toDoList);
         }
-
+        /// <summary>
+        /// Action that creates a ToDoList
+        /// </summary>
+        /// <param name="toDoList">Newly created toDoList from the body</param>
+        /// <returns>CreatedAtRoute status and redirects to a get</returns>
         [HttpPost]
-        public async Task<IActionResult> Create(ToDoList toDoList)
+        public async Task<IActionResult> Create([FromBody]ToDoList toDoList)
         {
             await _context.ToDoLists.AddAsync(toDoList);
             await _context.SaveChangesAsync();
 
             return CreatedAtRoute("GetList", new { id = toDoList.ID }, toDoList);
         }
-
+        /// <summary>
+        /// Action that updates the give list
+        /// </summary>
+        /// <param name="id">Id of list</param>
+        /// <param name="list">ToDoList object with data from the body</param>
+        /// <returns>NoContent if succeeded</returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute]int id, [FromBody]ToDoList list)
         {
@@ -64,7 +84,12 @@ namespace ToDoApi.Controllers
             await _context.SaveChangesAsync();
             return NoContent();
         }
-
+        /// <summary>
+        /// Action that will remove specific list and all todos
+        /// associated with that list
+        /// </summary>
+        /// <param name="id">Id of the list</param>
+        /// <returns>NotFound if null or NoContent if success</returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute]int id)
         {
